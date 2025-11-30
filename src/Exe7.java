fe4a8c75a92e7f224f2662991750b122e0fef742
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-30 14:04:46 +0200

    Add Exe7

---
+++ src/Exe7.java
@@ -0,0 +1,9 @@
+public class Exe7 {
+    public static void main(String[] args) {
+
+        String texte = "   Test la Java   ";
+        String faraSpatii = texte.replace(" ","");
+
+        System.out.println(faraSpatii);
+    }
+}
