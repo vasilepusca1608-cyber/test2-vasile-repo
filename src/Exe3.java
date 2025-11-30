5d7b17ed19940530b727914c8e93b88b217c41bc
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-30 12:57:39 +0200

    Add Exe3

---
+++ src/Exe3.java
@@ -0,0 +1,10 @@
+public class Exe3 {
+    public static void main(String[] args) {
+
+        int i = 19;//start
+        while (i < 40){  //conditie
+            i++; //iterator adauga o unitate +1
+            System.out.println(i); //printare
+        } //sfarsitul corpului buclei
+    }
+}
