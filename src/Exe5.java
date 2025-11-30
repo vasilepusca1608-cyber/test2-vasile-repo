aa9a3684ddc40ec5cf894829098f050e4759be5a
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-30 13:38:46 +0200

    Add Exe5

---
+++ src/Exe5.java
@@ -0,0 +1,12 @@
+public class Exe5 {
+    public static void main(String[] args) {
+
+        int[] numere = {1,2,3,4,5};
+        int suma = 0;
+        for (int i = 0; i < numere.length; i++){
+            suma = suma +numere[i];
+        }
+        System.out.println("Suma elementelor array-ului este: " + suma);
+
+    }
+}
