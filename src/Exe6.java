5331b47bb277a746262517c7e8b95f4879f7ca95
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-30 13:50:59 +0200

    Add Exe6

---
+++ src/Exe6.java
@@ -0,0 +1,11 @@
+public class Exe6 {
+    public static void main(String[] args) {
+
+        String s = "Azi avem test la limbajul de programare Java";
+        System.out.println(s);
+
+        System.out.println("Indicele lui A = " + s.indexOf('A'));
+
+        System.out.println("lastindexOf(a) = " + s.lastIndexOf('a'));
+    }
+}
