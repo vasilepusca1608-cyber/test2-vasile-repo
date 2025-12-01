458a110a9f5224f60731504d591d6c16b12dac49
Pusca Vasile <vasea-pusca@mail.ru>
2025-12-01 21:01:14 +0200

    Add Exe11

---
+++ src/Exe11.java
@@ -0,0 +1,11 @@
+public class Exe11 {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        do {
+            System.out.println("Bucla se executa odata. ");
+        } while (x < 5);
+        System.out.println("Rezultatul este: " + (x < 5));
+    }
+}
