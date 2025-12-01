93f4c067dce4937fe3250ce0f301a4190348ffeb
Pusca Vasile <vasea-pusca@mail.ru>
2025-12-01 20:32:22 +0200

    Add Exe9

---
+++ src/Exe9.java
@@ -0,0 +1,12 @@
+public class Exe9 {
+
+    public static int peremetrulPatrulater(){
+            int latura = 8;
+            int peremetru = 4 * latura;
+            return peremetru;
+        }
+        public static void main(String[] args) {
+        int rezultat = peremetrulPatrulater();
+        System.out.println("Peremetrul patratului este: " + rezultat + " cm");
+    }
+}
