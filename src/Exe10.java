c84a62d4d6eac965dec52959999628573641fbd3
Pusca Vasile <vasea-pusca@mail.ru>
2025-12-01 20:39:23 +0200

    Add Exe10

---
+++ src/Exe10.java
@@ -0,0 +1,14 @@
+public class Exe10 {
+        public static void main(String[] args) {
+
+            int i = 17;//start
+            while (i < 32){  //conditie
+                i++; //iterator adauga o unitate +1
+                if (i % 2 != 0){
+                System.out.println(i);} //printare
+            } //sfarsitul corpului buclei
+        }
+}
+
+
+
